7e4396fa6634be4d107e80dd237efa6ac3e08e34
Chamuth Chamandana <c.chamandana@gmail.com>
2020-04-12 00:08:51 +0530

    UI Fixes

--- lib/Screens/Register/register.dart
+++ lib/Screens/Register/register.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:grouped_buttons/grouped_buttons.dart';
 import 'package:touchable_opacity/touchable_opacity.dart';
 
@@ -13,10 +17,63 @@ enum AccountType { teacher, student }
 
 class _RegisterScreenState extends State<RegisterScreen> {
 
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailAddressController = TextEditingController(); 
+  final _phoneNumberController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
   AccountType _accType = AccountType.student;
+  final _registerFormKey = GlobalKey<FormState>();
+  bool registering = false;
+
+  void register() async
+  {
+    registering = true;
+
+    try
+    {
+      var result = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailAddressController.text, password: _passwordController.text);
+
+      var uid = result.user.uid;
+      await Firestore.instance.collection("users").document(uid).setData(
+        { 
+          'email' : _emailAddressController.text,
+          'first_name': _firstNameController.text,
+          'last_name' : _lastNameController.text, 
+          'phone' : _phoneNumberController.text, 
+          'teacher' : (_accType == AccountType.teacher),
+          'uid' : uid 
+        }
+      );
+
+      // user has been signed up
+
+
+    } catch (e) {
+      var message = "";
+      if (e.code == "ERROR_EMAIL_ALREADY_IN_USE")
+      {
+        message = "The email you entered is already associated with one of our accounts";
+      } else {
+        message = "An error occured. Please try again later.";
+      }
+
+      Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey,
+        textColor: Colors.white,
+        fontSize: 16.0
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    var confirmPasswordController = _confirmPasswordController;
     return Scaffold(
        appBar: AppBar(
          backgroundColor: Theme.of(context).backgroundColor,
@@ -28,11 +85,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
          centerTitle: true,
        ),
        body: Center(child:
-        Padding(child: 
           ListView(shrinkWrap: true, children: <Widget>[
+            Padding(child: Column(children: <Widget>[
             
-            Icon(Icons.person_outline, color: Colors.grey, size: 80),
-            Divider(height: 35, color: Colors.transparent),
+            Icon(Icons.person_outline, color: Colors.grey, size: 55),
+            Divider(height: 15, color: Colors.transparent),
 
             Align(alignment: Alignment.center, child: Text("Please select your role", style:TextStyle(fontSize: 18))),
             Divider(height: 15, color: Colors.transparent),
@@ -44,6 +101,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 child: Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0), child: 
                   Row(children: <Widget>[
                     Radio(
+                      activeColor: Theme.of(context).primaryColor,
                       value: AccountType.student,
                       groupValue: _accType,
                       onChanged: (AccountType value) {
@@ -61,6 +119,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 child: Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0), child: 
                   Row(children: <Widget>[
                     Radio(
+                      activeColor: Theme.of(context).primaryColor,
                       value: AccountType.teacher,
                       groupValue: _accType,
                       onChanged: (AccountType value) {
@@ -75,85 +134,169 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
             ],),
 
-            Divider(height: 20, color: Colors.transparent),
+            Divider(height: 10, color: Colors.transparent),
+            
+            Form(key: _registerFormKey, child: Column(children: <Widget>[
 
-            Padding(child: 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'First name',
-                  hintStyle: TextStyle(fontSize: 18),
-                ),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 18)
-              ), 
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
+              Padding(child: 
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'First name',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 18),
+                  controller: _firstNameController,
+                  validator: (value)
+                  {
+                    if (value.isEmpty)
+                    {
+                      return "Please enter your first name here";
+                    }
+                    
+                    return null;
+                  },
+                ), 
+              padding: EdgeInsets.fromLTRB(0, 2, 0, 2)),
 
-            Padding(child: 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Last name',
-                  hintStyle: TextStyle(fontSize: 18),
-                ),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 18)
-              ), 
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
+              Padding(child: 
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Last name',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 18),
+                  controller: _lastNameController,
+                  validator: (value)
+                  {
+                    if (value.isEmpty)
+                    {
+                      return "Please enter your last name here";
+                    }
+                    
+                    return null;
+                  },
+                ), 
+              padding: EdgeInsets.fromLTRB(0, 2, 0, 2)),
 
-            Padding(child: 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email address',
-                  hintStyle: TextStyle(fontSize: 18),
-                ),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 18)
-              ), 
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
+              Padding(child: 
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email address',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 18),
+                  controller: _emailAddressController,
+                  validator: (value)
+                  {
+                    if (value.isEmpty)
+                    {
+                      return "Please enter your email address";
+                    } else if (!EmailValidator.validate(value))
+                    {
+                      return "Please enter a valid email address";
+                    }
+                    
+                    return null;
+                  },
+                ), 
+              padding: EdgeInsets.fromLTRB(0, 2, 0, 2)),
 
-            Padding(child: 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Phone Number',
-                  hintStyle: TextStyle(fontSize: 18),
-                ),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.phone,
-                style: TextStyle(fontSize: 18)
-              ), 
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
+              Padding(child: 
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Phone Number',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.phone,
+                  style: TextStyle(fontSize: 18),
+                  controller: _phoneNumberController,
+                  validator: (value)
+                  {
+                    if (value.isEmpty)
+                    {
+                      return "Please enter your phone number";
+                    } else if (!((value.length == 10 && value.startsWith("0")) || (value.length == 11 && value.startsWith("94")) || (value.length == 12 && value.startsWith("+94"))))
+                    {
+                      return "Please enter a valid phone number";
+                    }
+                    
+                    return null;
+                  },
+                ), 
+              padding: EdgeInsets.fromLTRB(0, 2, 0, 2)),
 
-            Divider(height:20, color:Colors.transparent),
+              Divider(height:11, color:Colors.transparent),
 
-            Padding(child: 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: TextStyle(fontSize: 18),
-                ),
-                textInputAction: TextInputAction.next,
-                obscureText: true,
-                style: TextStyle(fontSize: 18)
-              ), 
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
+              Padding(child: 
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  obscureText: true,
+                  style: TextStyle(fontSize: 18),
+                  controller: _passwordController,
+                  validator: (value)
+                  {
+                    if (value.length == 0)
+                      return "Please enter a password";
+                    if (value.length < 5)
+                      return "The password isn't strong enough";
 
-            Padding(child: 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Confirm Password',
-                  hintStyle: TextStyle(fontSize: 18),
-                ),
-                textInputAction: TextInputAction.next,
-                obscureText: true,
-                style: TextStyle(fontSize: 18)
-              ), 
-            padding: EdgeInsets.fromLTRB(0, 5, 0, 5)),
+                    return null;
+                  },
+                ), 
+              padding: EdgeInsets.fromLTRB(0, 2, 0, 2)),
 
-        ],), padding: EdgeInsets.fromLTRB(30, 0, 30, 0)
-        )
+              Padding(child: 
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    hintStyle: TextStyle(fontSize: 18),
+                  ),
+                  textInputAction: TextInputAction.next,
+                  obscureText: true,
+                  style: TextStyle(fontSize: 18),
+                  controller: confirmPasswordController,
+                  validator: (value)
+                  {
+                    if (value.length == 0)
+                      return "Please confirm your password";
+
+                    if (value != _passwordController.text)
+                      return "Passwords don't match";
+
+                    return null;
+                  }
+                ), 
+              padding: EdgeInsets.fromLTRB(0, 5, 0, 35)),
+
+              Row(children: <Widget>[
+                Expanded(child: RaisedButton(child: Text("CREATE ACCOUNT"), onPressed: () {
+                    if (_registerFormKey.currentState.validate()) {
+                      register();
+                    }
+                 },))
+              ],),
+
+              Divider(height:20, color:Colors.transparent),
+
+            ]))
+
+        ],), padding: EdgeInsets.fromLTRB(30, 0, 30, 0))])
+  
       ,)
     );
   }
+}
+
+class _confirmPasswordController {
 }
